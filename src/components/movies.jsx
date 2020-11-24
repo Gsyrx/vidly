ee6cc1e4f67385edf611588d3994f59a02b7a1fd
Gaurav Kumar <gauravsyr10@gmail.com>
2020-11-24 05:56:53 +0530

    completed pagination component interface

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,89 @@
+// import React, { Component } from 'react';
+// import Like from './common/like';
+// import { getMovies } from '../services/fakeMovieService';
+
+// class Movies extends Component {
+//   state = {
+//     movies: getMovies(),
+//   };
+
+//   handleDelete = (movie) => {
+//     // console.log(movie);
+
+//     // filter returns a new array with the passed condition(ie the condition which satisfy)
+//     const movies = this.state.movies.filter((m) => m._id !== movie._id);
+//     this.setState({ movies });
+//   };
+
+//   handleLike = (movie) => {
+//     // here parameter 'movie' is the clicked movie(object) (ie on like button)
+//     const movies = [...this.state.movies];
+//     const index = movies.indexOf(movie);
+//     movies[index] = { ...movies[index] };
+//     movies[index].liked = !movies[index].liked;
+//     this.setState({ movies });
+//   };
+
+//   render() {
+//     const { length: count } = this.state.movies;
+//     if (count === 0) return <p>There are no movies in the database!</p>;
+//     return (
+//       <React.Fragment>
+//         <p>Showing the {count} movies in the database</p>
+//         <table className="table">
+//           <thead>
+//             <tr>
+//               <th>Title</th>
+//               <th>Genre</th>
+//               <th>Stock</th>
+//               <th>Rate</th>
+//               <th />
+//               <th />
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {this.state.movies.map((movie) => (
+//               <tr key={movie._id}>
+//                 <td>{movie.title}</td>
+//                 <td>{movie.genre.name}</td>
+//                 <td>{movie.numberInStock}</td>
+//                 <td>{movie.dailyRentalRate}</td>
+//                 <td>
+//                   <Like
+//                     liked={movie.liked}
+//                     onClick={() => this.handleLike(movie)}
+//                   />
+//                 </td>
+//                 <td>
+//                   <button
+//                     onClick={() => this.handleDelete(movie)}
+//                     className="btn btn-danger btn-sm"
+//                   >
+//                     Delete
+//                   </button>
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default Movies;
+
+// < ----- Pagination Filtering and Sorting ----- >
+
 import React, { Component } from 'react';
 import Like from './common/like';
+import Pagination from './common/pagination';
 import { getMovies } from '../services/fakeMovieService';
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -22,6 +101,10 @@ class Movies extends Component {
     movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+    console.log(page);
   };
 
   render() {
@@ -66,6 +149,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
