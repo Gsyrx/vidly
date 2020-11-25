ef3aaf6877ab87089373f4936c4bebc1e94b07cc
Gaurav Kumar <gauravsyr10@gmail.com>
2020-11-25 08:51:39 +0530

    completed displaying items

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,8 +1,17 @@
 import React from 'react'; // imr
 
 // sfc
-const ListGroup = () => {
-  return null;
+const ListGroup = (props) => {
+  const { items, textProperty, valueProperty } = props;
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li key={item[valueProperty]} className="list-group-item">
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListGroup;
