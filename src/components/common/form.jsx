07f5c565b427acd7bc54c9929d7f923defd7a69d
Gaurav Kumar <gauravsyr10@gmail.com>
2020-12-12 23:46:12 +0530

    refactoring and extraction of a reusable form is done

---
+++ src/components/common/form.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import Joi from 'joi-browser';
+import Input from './input';
+
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {},
+  };
+
+  validate = () => {
+    // only for demo to find the properties
+    // const result = Joi.validate(this.state.data, this.schema)
+    // console.log(result);
+
+    const options = { abortEarly: false };
+
+    // error is one of the property of Joi.validate....
+    const { error } = Joi.validate(this.state.data, this.schema, options);
+
+    // here we are returning null because if there is no error
+    // then there will be no error property in Joi.valiadate()
+    if (!error) return null;
+
+    // error :- details (array)
+    // details[] :- [(0: message: "...", path: ["username"]),(1: message: "...", path: ["password"]) .... ]
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+    // const errors = {};
+
+    // const { username, password } = this.state.data;
+
+    // if (username.trim() === '') errors.username = 'Username is required!';
+    // if (password.trim() === '') errors.password = 'Password is required!';
+
+    // // Object.keys() return an array of keys only not values
+    // return Object.keys(errors).length === 0 ? null : errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+
+    // if (name === 'username') {
+    //   if (value.trim() === '') return 'Username is required';
+    // }
+    // if (name === 'password') {
+    //   if (value.trim() === '') return 'Password is required';
+    // }
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // this.validate() will validate the entire form
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    this.doSubmit();
+  };
+
+  // handleChange = (e) => {
+  //   const data = { ...this.state.data };
+  //   data[e.currentTarget.name] = e.currentTargetvalue;
+  //   this.setState({ data });
+  // };
+
+  handleChange = ({ currentTarget: input }) => {
+    // console.log(input);
+    const errors = { ...this.state.errors };
+
+    // this.validateProperty() will validate only the input field
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    // here delete is used to remove the input error message
+    // when the user after typing delete it,
+    // if we not use delete then after first typing and deleting the content
+    // the error message will be displayed all the time
+    else delete errors[input.name];
+
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+
+    this.setState({ data, errors });
+  };
+}
+
+export default Form;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Joi from 'joi-browser';
+import Form from './common/form';
 import Input from './common/input';
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: { username: '', password: '' },
+    data: { username: '', password: '' },
     errors: {},
   };
 
@@ -14,104 +15,35 @@ class LoginForm extends Component {
     password: Joi.string().required().label('Password'),
   };
 
-  validate = () => {
-    // only for demo to find the properties
-    // const result = Joi.validate(this.state.account, this.schema)
-    // console.log(result);
-
-    const options = { abortEarly: false };
-
-    // error is one of the property of Joi.validate....
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-
-    // here we are returning null because if there is no error
-    // then there will be no error property in Joi.valiadate()
-    if (!error) return null;
-
-    // error :- details (array)
-    // details[] :- [(0: message: "...", path: ["username"]),(1: message: "...", path: ["password"]) .... ]
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-    // const errors = {};
-
-    // const { username, password } = this.state.account;
-
-    // if (username.trim() === '') errors.username = 'Username is required!';
-    // if (password.trim() === '') errors.password = 'Password is required!';
-
-    // // Object.keys() return an array of keys only not values
-    // return Object.keys(errors).length === 0 ? null : errors;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    // this.validate() will validate the entire form
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
     // call the server
     console.log('Submited');
   };
 
-  // handleChange = (e) => {
-  //   const account = { ...this.state.account };
-  //   account[e.currentTarget.name] = e.currentTargetvalue;
-  //   this.setState({ account });
-  // };
-
-  validateProperty = ({ name, value }) => {
-    // if (name === 'username') {
-    //   if (value.trim() === '') return 'Username is required';
-    // }
-    // if (name === 'password') {
-    //   if (value.trim() === '') return 'Password is required';
-    // }
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    // console.log(input);
-    const errors = { ...this.state.errors };
-
-    // this.validateProperty() will validate only the input field
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    // here delete is used to remove the input error message
-    // when the user after typing delete it,
-    // if we not use delete then after first typing and deleting the content
-    // the error message will be displayed all the time
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <Input
             name="username"
-            value={account.username}
+            value={data.username}
             label="Username"
             onChange={this.handleChange}
             error={errors.username}
           />
           <Input
             name="password"
-            value={account.password}
+            value={data.password}
             label="Password"
             onChange={this.handleChange}
             error={errors.password}
           />
 
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
