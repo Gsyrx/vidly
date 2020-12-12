1a4df56016e2c1d2c2ce4629cbc15014572d1c7f
Gaurav Kumar <gauravsyr10@gmail.com>
2020-12-13 00:47:41 +0530

    added search column

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -309,44 +309,171 @@
 
 // < ----- Sorting ----- >
 
+// import React, { Component } from 'react';
+// import MoviesTable from './moviesTable';
+// import ListGroup from './common/listGroup';
+// import Pagination from './common/pagination';
+// import { getMovies } from '../services/fakeMovieService';
+// import { getGenres } from '../services/fakeGenreService';
+// import { paginate } from '../utils/paginate';
+// import _ from 'lodash'; // for sorting
+
+// class Movies extends Component {
+//   state = {
+//     // these properties(movies, genres) are initialised to empty array because
+//     // it's going to take some time until we get the data from the server,
+//     // during this time we make sure that 'movies' and 'genres' are not undefined
+//     // otherwise we will get a runtime error
+//     movies: [],
+//     genres: [],
+//     currentPage: 1,
+//     pageSize: 4,
+//     sortColumn: { path: 'title', order: 'asc' },
+//   };
+
+//   // this method will be called when an instance of this component is rendered in the DOM
+//   componentDidMount() {
+//     const genres = [{ _id: '', name: 'All Genres' }, ...getGenres()];
+//     this.setState({ movies: getMovies(), genres });
+//   }
+
+//   handleDelete = (movie) => {
+//     // console.log(movie);
+
+//     // filter returns a new array with the passed condition(ie the condition which satisfy)
+//     const movies = this.state.movies.filter((m) => m._id !== movie._id);
+//     this.setState({ movies });
+//   };
+
+//   handleLike = (movie) => {
+//     // here parameter 'movie' is the clicked movie(object) (ie on like button)
+//     const movies = [...this.state.movies];
+//     const index = movies.indexOf(movie);
+//     movies[index] = { ...movies[index] };
+//     movies[index].liked = !movies[index].liked;
+//     this.setState({ movies });
+//   };
+
+//   handlePageChange = (page) => {
+//     this.setState({ currentPage: page });
+//   };
+
+//   handleGenreSelect = (genre) => {
+//     // {_id: "5b21ca3eeb7f6fbccd471818", name: "Action"}
+//     // console.log(genre);
+
+//     this.setState({ selectedGenre: genre, currentPage: 1 });
+//   };
+
+//   handleSort = (sortColumn) => {
+//     // click on 'Title' -> title
+//     // click on 'Genre' -> genre.name
+//     // console.log(path);
+
+//     this.setState({ sortColumn });
+//   };
+
+//   getPagedData = () => {
+//     const {
+//       pageSize,
+//       currentPage,
+//       sortColumn,
+//       selectedGenre,
+//       movies: allMovies,
+//     } = this.state;
+
+//     // (selectedGenre && selectedGenre._id) both are selected because
+//     // without 'selectedGenre._id' 'All Genres' will show 0 movies,
+//     // because 'All Genres' have no id, so we use && to filter out only those
+//     // which have satisfied both condition
+
+//     const filtered =
+//       selectedGenre && selectedGenre._id
+//         ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+//         : allMovies;
+
+//     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+//     const movies = paginate(sorted, currentPage, pageSize);
+
+//     return { totalCount: filtered.length, data: movies };
+//   };
+
+//   render() {
+//     const { length: count } = this.state.movies;
+//     const { pageSize, currentPage, sortColumn } = this.state;
+//     if (count === 0) return <p>There are no movies in the database!</p>;
+
+//     const { totalCount, data: movies } = this.getPagedData();
+
+//     return (
+//       <div className="row">
+//         <div className="col-3">
+//           <ListGroup
+//             items={this.state.genres}
+//             selectedItem={this.state.selectedGenre}
+//             onItemSelect={this.handleGenreSelect}
+//           />
+//         </div>
+//         <div className="col">
+//           <p>Showing the {totalCount} movies in the database</p>
+//           <MoviesTable
+//             movies={movies}
+//             sortColumn={sortColumn}
+//             onLike={this.handleLike}
+//             onDelete={this.handleDelete}
+//             onSort={this.handleSort}
+//           />
+//           <Pagination
+//             itemsCount={totalCount}
+//             pageSize={pageSize}
+//             currentPage={currentPage}
+//             onPageChange={this.handlePageChange}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Movies;
+
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MoviesTable from './moviesTable';
 import ListGroup from './common/listGroup';
 import Pagination from './common/pagination';
-import { getMovies } from '../services/fakeMovieService';
+import { getMovies, deleteMovie } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService';
 import { paginate } from '../utils/paginate';
-import _ from 'lodash'; // for sorting
+import _ from 'lodash';
+import SearchBox from './searchBox';
 
 class Movies extends Component {
   state = {
-    // these properties(movies, genres) are initialised to empty array because
-    // it's going to take some time until we get the data from the server,
-    // during this time we make sure that 'movies' and 'genres' are not undefined
-    // otherwise we will get a runtime error
     movies: [],
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    searchQuery: '',
+    selectedGenre: null,
     sortColumn: { path: 'title', order: 'asc' },
   };
 
-  // this method will be called when an instance of this component is rendered in the DOM
   componentDidMount() {
     const genres = [{ _id: '', name: 'All Genres' }, ...getGenres()];
+
     this.setState({ movies: getMovies(), genres });
   }
 
   handleDelete = (movie) => {
-    // console.log(movie);
-
-    // filter returns a new array with the passed condition(ie the condition which satisfy)
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
+
+    deleteMovie(movie._id);
   };
 
   handleLike = (movie) => {
-    // here parameter 'movie' is the clicked movie(object) (ie on like button)
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
     movies[index] = { ...movies[index] };
@@ -359,17 +486,14 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    // {_id: "5b21ca3eeb7f6fbccd471818", name: "Action"}
-    // console.log(genre);
+    this.setState({ selectedGenre: genre, searchQuery: '', currentPage: 1 });
+  };
 
-    this.setState({ selectedGenre: genre, currentPage: 1 });
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
   };
 
   handleSort = (sortColumn) => {
-    // click on 'Title' -> title
-    // click on 'Genre' -> genre.name
-    // console.log(path);
-
     this.setState({ sortColumn });
   };
 
@@ -379,18 +503,17 @@ class Movies extends Component {
       currentPage,
       sortColumn,
       selectedGenre,
+      searchQuery,
       movies: allMovies,
     } = this.state;
 
-    // (selectedGenre && selectedGenre._id) both are selected because
-    // without 'selectedGenre._id' 'All Genres' will show 0 movies,
-    // because 'All Genres' have no id, so we use && to filter out only those
-    // which have satisfied both condition
-
-    const filtered =
-      selectedGenre && selectedGenre._id
-        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-        : allMovies;
+    let filtered = allMovies;
+    if (searchQuery)
+      filtered = allMovies.filter((m) =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+    else if (selectedGenre && selectedGenre._id)
+      filtered = allMovies.filter((m) => m.genre._id === selectedGenre._id);
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
@@ -401,8 +524,9 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, sortColumn } = this.state;
-    if (count === 0) return <p>There are no movies in the database!</p>;
+    const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+
+    if (count === 0) return <p>There are no movies in the database.</p>;
 
     const { totalCount, data: movies } = this.getPagedData();
 
@@ -416,7 +540,15 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Showing the {totalCount} movies in the database</p>
+          <Link
+            to="/movies/new"
+            className="btn btn-primary"
+            style={{ marginBottom: 20 }}
+          >
+            New Movie
+          </Link>
+          <p>Showing {totalCount} movies in the database.</p>
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
