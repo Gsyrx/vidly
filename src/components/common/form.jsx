34d584dd60ae77f43a1c93108b773915db16dc07
Gaurav Kumar <gauravsyr10@gmail.com>
2020-12-13 00:16:10 +0530

    register form component is done

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -87,6 +87,36 @@ class Form extends Component {
 
     this.setState({ data, errors });
   };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderInput(name, label, type = 'text') {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        type={type}
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+      // <Input
+      //   name="username"
+      //   value={data.username}
+      //   label="Username"
+      //   onChange={this.handleChange}
+      //   error={errors.username}
+      // />
+    );
+  }
 }
 
 export default Form;
